01988fe93a7f35fd2968cda2748e7fae71443176
Colin Fendrick <colin.fendrick@gmail.com>
2020-12-05 05:14:01 -0800

    At beginning and end adds

--- linked-lists/linked-lists.py
+++ linked-lists/linked-lists.py
@@ -13,15 +13,32 @@ class LinkedList():
             print (printval.dataval)
             printval = printval.nextval
 
-list1 = LinkedList()
-list1.headval = Node("Mon")
+    def AtBegining(self, newdata):
+        NewNode = Node(newdata)
+
+        # Update the new nodes next val to existing node
+        NewNode.nextval = self.headval
+        self.headval = NewNode
+
+    def AtEnd(self, newdata):
+        NewNode = Node(newdata)
+        if self.headval is None:
+            self.headval = NewNode
+            return
+        laste = self.headval
+        while(laste.nextval):
+            laste = laste.nextval
+        laste.nextval=NewNode
+
+lst = LinkedList()
+lst.headval = Node("Mon")
 e2 = Node("Tue")
 e3 = Node("Wed")
 
-# Link first Node to second node
-list1.headval.nextval = e2
-
-# Link second Node to third node
+lst.headval.nextval = e2
 e2.nextval = e3
 
-list1.listprint()
+lst.AtBegining("Sun")
+lst.AtEnd("Thurs")
+
+lst.listprint()
