2c8f300282b23357aa71ecd16e643083f912ac68
Colin Fendrick <colin.fendrick@gmail.com>
2020-12-05 06:01:21 -0800

    Add a way to iterate a list into a linkedlist

--- linked-lists/linked-lists.py
+++ linked-lists/linked-lists.py
@@ -1,32 +1,32 @@
 class Node:
-    def __init__(self, dataval=None):
-        self.dataval = dataval
-        self.nextval = None
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
 
 class LinkedList():
     def __init__(self):
-        self.headval = None
+        self.head = None
 
     def listprint(self):
-        printval = self.headval
+        printval = self.head
         while printval is not None:
-            print (printval.dataval)
-            printval = printval.nextval
+            print (printval.data)
+            printval = printval.next
 
     def AtBegining(self, newdata):
         NewNode = Node(newdata)
-        NewNode.nextval = self.headval
-        self.headval = NewNode
+        NewNode.next = self.head
+        self.head = NewNode
 
     def AtEnd(self, newdata):
         NewNode = Node(newdata)
-        if self.headval is None:
-            self.headval = NewNode
+        if self.head is None:
+            self.head = NewNode
             return
-        laste = self.headval
-        while(laste.nextval):
-            laste = laste.nextval
-        laste.nextval=NewNode
+        laste = self.head
+        while(laste.next):
+            laste = laste.next
+        laste.next=NewNode
 
     def Inbetween(self, middle_node, newdata):
         if middle_node is None:
@@ -34,19 +34,54 @@ class LinkedList():
             return
 
         NewNode = Node(newdata)
-        NewNode.nextval = middle_node.nextval
-        middle_node.nextval = NewNode
+        NewNode.next = middle_node.next
+        middle_node.next = NewNode
+
+    def RemoveNode(self, Removekey):
+        Head = self.head
+
+        if (Head is not None):
+            if (Head.data == Removekey):
+                self.head = Head.next
+                Head = None
+                return
+
+        while (Head is not None):
+            if Head.data == Removekey:
+                break
+            prev = Head
+            Head = Head.next
+
+        if (Head == None):
+            return
+
+        prev.next = Head.next
+        Head = None
+
+def AddList(list, llist = None):
+    if (llist is None):
+        llist = LinkedList()
+    
+    for el in list:
+        llist.AtEnd(el)
+    
+    return llist
 
 
-lst = LinkedList()
-lst.headval = Node("Mon")
-lst.headval.nextval = Node("Tue")
 
-lst.AtBegining("Sun")
-lst.AtEnd("Sat")
-lst.Inbetween(lst.headval.nextval.nextval, "Wed")
-lst.Inbetween(lst.headval.nextval.nextval.nextval, "Thurs")
-lst.Inbetween(lst.headval.nextval.nextval.nextval.nextval, "Fri")
-
-
+lst = AddList([1, 4, 5, 6])
+lst.Inbetween(lst.head, 2)
+lst.Inbetween(lst.head.next, 3)
 lst.listprint()
+
+# lst.head = Node("Mon")
+# lst.head.next = Node("Tue")
+
+# lst.AtBegining("Sun")
+# lst.AtEnd("Sat")
+
+# # This is a dumb way to do this, obviously just can insert these into the end but hey wygd
+# lst.Inbetween(lst.head.next.next, "Wed")
+# lst.Inbetween(lst.head.next.next.next, "Thurs")
+# lst.Inbetween(lst.head.next.next.next.next, "Fri")
+# lst.RemoveNode("Tue")
