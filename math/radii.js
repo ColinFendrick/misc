93ccafb6a11d501c950af946754d83de5c11d521
Colin Fendrick <colin.fendrick@gmail.com>
2021-02-14 07:16:30 -0800

    pizza sizes

---
+++ math/radii.js
@@ -0,0 +1,4 @@
+const circleArea = r => Math.PI * (r ** 2);
+const compareArea = (r1, r2) => r2 > r1 ? [r2 / r1, circleArea(r2) / circleArea(r1)] : [r1 / r2, circleArea(r1) / circleArea(r2)];
+
+console.log(compareArea(9, 10));
