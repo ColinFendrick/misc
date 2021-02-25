f76714e07d4d16eccbae139946f38f2e31e125c1
Colin Fendrick <colin.fendrick@gmail.com>
2021-02-25 14:07:48 -0800

    wip

--- math/nonLinearDistance.js
+++ math/nonLinearDistance.js
@@ -16,7 +16,37 @@ const distanceMetric = (a, b) => Math.sqrt(
 
 const onanism = metricSpace(coordinates, distanceMetric);
 
-const c1 = [0,0,0,0];
-const c2 = [1,1,1,1];
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-trace('onanism')(`${c1}, ${c2} : ${apply(onanism)(c1, c2)}`);
+const origin = [0, 0, 0, 0];
+let c1 = 0;
+let c2 = 0;
+let c3 = 0;
+let c4 = 0;
+
+const q1 = () => readline.question('did you masturbate today? Please answer yes or no. \n', ans => {
+  ans = ans.toLowerCase();
+  if (ans === 'no') {
+    console.log('Good for you.');
+    readline.close();
+  } else if (ans === 'yes') {
+    console.log('Bad boy');
+    c1 = 1;
+    readline.close();
+  } else {
+    console.log('Your answer is incorrectly formatted, please try again.');
+    q1();
+  }
+});
+q1();
+
+const terminus = [c1, c2, c3, c4];
+
+console.log(
+  apply(onanism)(origin, terminus)
+)
+
+// trace('onanism')(`${origin}, ${terminus} : ${apply(onanism)(origin, terminus)}`);
